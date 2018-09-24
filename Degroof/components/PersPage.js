0fbf2a3324999b4ffc61c99a8aacddce9fee1ef1
Alex <infanti.alexandre@gmail.com>
2018-09-24 16:44:59 +0200

    push personal page

---
+++ Degroof/components/PersPage.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import {View,Text, Image, StyleSheet} from 'react-native';
+
+
+class Boilerplate extends React.Component{
+	render(){
+		return(
+			 <View style={styles.page}>
+                <Text style={styles.maintitle}> Personal Page</Text>
+                <Image source={require('../assets/images/userLog.png')} />
+                <Text style={styles.hi}>Hi PSEUDO !</Text>
+                <View style={styles.blocinfo}>
+                    <Text style={styles.infotitle}> My informations :</Text>
+                    <View style={styles.infobloc}>
+                        <Text style={styles.infosubtitle}>Adress</Text>
+                        <Text>Un truc</Text>
+                    </View>
+                    <View style={styles.infobloc}>
+                        <Text style={styles.infosubtitle}>Region</Text>
+                        <Text>Un autre truc</Text>  
+                    </View>
+                    <View style={styles.infobloc}>
+                        <Text style={styles.infosubtitle}>Skills</Text>
+                        <Text>Une liste de ouf</Text>
+                        <Text>Une liste de ouf</Text>
+                        <Text>Une liste de ouf</Text>
+                        <Text>Une liste de ouf</Text>
+
+                    </View>
+                </View>
+        	</View>
+
+			)
+	}
+}
+export default Boilerplate;
+
+const styles=StyleSheet.create({
+    page:{
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingTop: 50,
+    },
+
+	maintitle:{
+        textAlign: 'center',
+        fontWeight: 'bold',
+        fontSize: 25,
+    },
+
+    hi:{
+        paddingBottom: 50,
+    },
+
+    blocinfo: {
+        width: '80%',
+        backgroundColor: '#00cd76',
+        borderRadius: 25,
+        alignItems: 'center',
+
+    },
+
+    infotitle: {
+        fontWeight: 'bold',
+        fontSize: 20,
+        paddingVertical: 15,
+    },
+
+    infobloc: {
+        paddingBottom: 15,
+    },
+
+
+    infosubtitle: {
+        fontWeight: 'bold',
+        textAlign: 'center',
+        color: 'white',
+        
+    }
+	})

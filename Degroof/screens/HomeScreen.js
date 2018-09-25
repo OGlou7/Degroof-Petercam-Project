ba0df66b01a3e67d6e43b0e81cd820ab7f651db4
NathanHoutain <nathan-houtain@outlook.com>
2018-09-25 16:48:43 +0200

    searchBar

--- Degroof/screens/HomeScreen.js
+++ Degroof/screens/HomeScreen.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
-import { StyleSheet, View, Text} from 'react-native'
+import { StyleSheet, View, Text, TextInput} from 'react-native'
 import ProjectCard from '../components/ProjectCard'
+import { SearchBar } from 'react-native-elements'
 
 const projectData = [
   {
@@ -110,9 +111,14 @@ export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
+        <SearchBar
+        lightTheme
+        // onChangeText={someMethod}
+        // onClearText={someMethod}
+        placeholder='Type Here...' />
         <ProjectCard data={projectData} />
       </View>
     )
   }
 }
-const styles = StyleSheet.create({ container: { flex: 1, marginTop: 50 } })
+const styles = StyleSheet.create({ container: { flex: 1, marginTop: 24 } })
